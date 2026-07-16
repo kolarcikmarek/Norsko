6e8124e4a22df1710a7f3ae09f984d80c1de90e7
kolarcikmarek <twelve@twelve.sk>
2026-07-17 01:13:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "norsko26-v9";
+const CACHE = "norsko26-v11";
 const SHELL = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
